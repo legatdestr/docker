d3bee5dac577aa0de0f02b247633363e93262e3e
ksb <ksb@em70.ru>
2017-05-19 16:01:42 +0700

    web server bash script added

--- start.sh
+++ start.sh
@@ -22,3 +22,6 @@ docker run -it --network="bridge" \
     -w "${CONTAINER_WORKDIR}" \
     legatdestr/ksb-yo-webapp \
     /bin/bash ;
+
+
+echo 'Выполнена команда запуска веб-сервера. В консоль будет выводиться лог запросов.';
