9f6440af629f5fda9bb5c7620835856ecccd1aba
ksb <ksb@em70.ru>
2017-05-25 08:48:21 +0700

    start script improvements

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 CONTAINER_NAME='web_tablo';
-CONTAINER_HELP="Чтобы остановить контейнер, просто завершите текущий процесс в консоли. (Ctrl+C)";
+CONTAINER_HELP="Чтобы остановить контейнер, просто завершите текущий процесс в консоли. (Ctrl+C, Ctrl+D)";
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 TMP_DIR_NAME='container_tmp';
 TMP_DIR=${SCRIPT_DIR}/"${TMP_DIR_NAME}";
@@ -24,7 +24,7 @@ mkdir ${TMP_DIR} && mkdir ${LOGS_DIR} 2>/dev/null;
 chmod 777 ${HT_DOCS_DIR_PATH}/assets;
 chmod 777 ${PROTECTED_DIR_PATH}/runtime;
 
-docker run -it --network="bridge" \
+docker run -d --network="bridge" \
     -p80:80 \
     --rm \
     --name ${CONTAINER_NAME} \
@@ -35,5 +35,7 @@ docker run -it --network="bridge" \
     -v ${HT_TEMP_DIR_PATH}/../.ht_temp:/var/www/vhosts/er.em70.ru/.ht_temp \
     -v ${LOGS_DIR}:/var/www/vhosts/er.em70.ru/logs/ \
     legatdestr/ksb_web_tablo \
-    apache2-foreground && echo "Container ${CONTAINER_NAME} was stopped. Exiting..."
+    apache2-foreground
 
+docker exec -it ${CONTAINER_NAME} bash
+docker stop ${CONTAINER_NAME} && echo "Container ${CONTAINER_NAME} was stopped."
