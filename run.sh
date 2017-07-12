e19b161b6f85afd7bddbfd69d991db1f9da452ec
ksb <ksb@em70.ru>
2017-07-12 11:44:18 +0700

    dev without db

--- run.sh
+++ run.sh
@@ -3,7 +3,12 @@
 set -e
 source .env
 
-DOCKER_COMPOSE_FILE="docker-compose-dev.yml"
+
+DOCKER_COMPOSE_DB_FILE="docker-compose-db.yml"
+DOCKER_COMPOSE_DEV_FILE="docker-compose-dev.yml"
+# по умолчанию - dev, но может быть переопределен при старте
+DOCKER_COMPOSE_FILE=${DOCKER_COMPOSE_DEV_FILE}
+
 
 print_help() {
   echo "Работа с сервисом:"
